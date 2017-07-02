7e5276a6a5a0f4899a77ae155d4e3dd31904a83b
Madabaru <felixjo@web.de>
2017-07-02 09:29:43 +0200

    Update keras.py

--- keras.py
+++ keras.py
@@ -12,37 +12,6 @@ from keras import losses
 
 class NN():
 
-    def dataprep(self, state):
-        # Matrix-Generierung:
-        # Pacman_Position
-        width, height = state.data.layout.width,state.data.layout.height
-        matPacPosition = np.zeros((width, height),dtype= int)
-        x,y = state.getPacmanPostion()
-        matPacPosition[height - 1 - y,x] = 1
-
-
-        #Walls Position
-        matWallsPosition = np.zeros((width, height),dtype= int)
-        walls = state.getWalls()
-        for i in range(width):
-            for j in range(height):
-                if walls[i,j]:
-                    matWallsPosition[height -1 - j,i] = 1
-
-        #Ghost Positions:
-        #Comment: Sollen wir die Ghosts noch voneinander unterscheiden (oder ist das dann schon wieder eine High-Level
-        # Info)?
-        matGhostsPosition = np.zeros((width, height), dtype=int)
-        ghosts = state.getGhostPositions()
-
-        for g in ghosts:
-            x,y = g
-            matGhostsPosition[height - y - 1, x] = 1
-
-
-        return matPacPosition, matWallsPosition, matGhostsPosition
-
-
     def _init_(self, state):
          width, height = state.data.layout.width, state.data.layout.height
          self.action.size = 4;  #5 
