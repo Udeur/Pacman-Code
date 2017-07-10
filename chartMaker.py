7278e4efea76c3bd311b7bc1f44f9f3a6f2bf33b
yourFriendlyNeighbour <monsieur_pierre@my.mail.de>
2017-07-10 08:28:28 +0200

    chart maker

---
+++ chartMaker.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#numAr kann auch mehr dimensional sein.
+#Plot interpretiert automatisch zwei Spalten als x,y-Werte für eine Linie.
+#So können dann auch alle Gewichte angezeigt werden
+
+def createChart(numAr, title, ylabel):
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.figure(1)
+    plt.plot(numAr)
+    plt.show()
+
+if __name__ == '__main__':
+    yvalues = np.random.rand(100)
+    createChart(yvalues, 'Testtitel', 'Gewicht')
